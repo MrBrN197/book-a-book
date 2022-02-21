6a630ed2a6ad20ff6d55915286c368b278225026
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-21 12:22:25 +0100

    Add validations to the reservation moel

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :reservation_date, presence: true, availability: true
+  validates :city, presence: true, length: { maximum: 250 }
 end
