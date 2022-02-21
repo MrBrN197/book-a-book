bfaebe00de8eb3734e94f4aefed92e589a1aae03
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-21 09:53:09 +0100

    Configure json output for the index action of the reservation's controller

---
+++ app/controllers/api/reservations_controller.rb
@@ -0,0 +1,11 @@
+class Api::ReservationsController < ApplicationController
+  def index
+    reservations = current_user.reservations.includes(:book)
+    render json: reservations, except: [:created_at, :updated_at]
+  end
+
+  # def show
+  #   reservation = current_user.reservations.find(params[:id])
+  #   render json: reservation, except: [:created_at, :updated_at]
+  # end
+end
