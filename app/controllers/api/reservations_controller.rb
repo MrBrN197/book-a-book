1ef5f639863f183fcd7f883b7b9dfe1dd999a11d
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-21 10:41:51 +0100

    Write a method to create a new reservation

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -8,4 +8,19 @@ class Api::ReservationsController < ApplicationController
     reservation = current_user.reservations.find(params[:id])
     render json: reservation, except: [:created_at, :updated_at]
   end
+
+  def create
+    new_reservation = current_user.reservations.new(reservation_params)
+    if new_reservation.save
+      render json: new_reservation, message: 'Reservation Created', except: [:created_at, :updated_at]
+    else
+      render json: new_reservation.errors, status: :bad_request, message: 'Operation failed'
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.permit(:reservation_date, :city, :user_id, :book_id)
+  end
 end
