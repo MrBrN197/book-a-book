8d7ebe82a80ddd02dc48dd5ee8db8b0a979f375b
onesis <Onesis.olea@gmail.com>
2022-02-21 12:39:23 -0400

    create books controlelr

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,51 +1,37 @@
-class BooksController < ApplicationController
-  before_action :set_book, only: %i[ show update destroy ]
-
-  # GET /books
+class Api::BooksController < ApplicationController
   def index
-    @books = Book.all
-
+    @books = Book.all.with_attached_image
     render json: @books
   end
 
-  # GET /books/1
-  def show
-    render json: @book
+  def show; 
+  @book = Book.find(params[:id])
+  render json: @book, status: :ok
   end
 
-  # POST /books
   def create
     @book = Book.new(book_params)
 
     if @book.save
-      render json: @book, status: :created, location: @book
+      render json: @book, status: :created
     else
       render json: @book.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /books/1
-  def update
-    if @book.update(book_params)
-      render json: @book
-    else
-      render json: @book.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /books/1
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
+    render json: { message: 'Book successfully deleted' }, status: :no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def book_params
-      params.fetch(:book, {})
-    end
+  def set_book
+    @book = Book.with_attached_image.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author, :image)
+  end
 end
