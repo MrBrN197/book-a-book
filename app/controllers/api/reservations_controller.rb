1b2ed3f5c9b523efac363dba4c86486e40132469
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-21 09:53:45 +0100

    Configure json output for the show action of the reservation's controller

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -4,8 +4,8 @@ class Api::ReservationsController < ApplicationController
     render json: reservations, except: [:created_at, :updated_at]
   end
 
-  # def show
-  #   reservation = current_user.reservations.find(params[:id])
-  #   render json: reservation, except: [:created_at, :updated_at]
-  # end
+  def show
+    reservation = current_user.reservations.find(params[:id])
+    render json: reservation, except: [:created_at, :updated_at]
+  end
 end
