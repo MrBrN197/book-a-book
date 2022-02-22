1375e93931a0ca59b64e1a827adf9b9a0c98ad1b
brianorora <brianorora@gmail.com>
2022-02-22 09:50:40 +0300

    Update database schema.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+book1 = Book.create!(
+    title: 'El Coronel no tiene quien le escriba',
+    author: 'Gabriel Garcia Marquez',
+    image: 'https://images.unsplash.com/photo-1589998059171-988d887df646?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3BlbiUyMGJvb2t8ZW58MHx8MHx8&w=1000&q=80'
+)
+
+user = User.create!(username: 'Matumba Ngambi')
+
+reservation1 = user.reservations.create!(
+    city: 'Lagos', 
+    reservation_date: DateTime.strptime("09/14/2009 8:00", "%m/%d/%Y %H:%M"),
+    book: book1
+)
