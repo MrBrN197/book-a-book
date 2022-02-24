e7ed3da7b3f769e10e7e24f8c1335ed4ab8cfde0
brianorora <brianorora@gmail.com>
2022-02-24 14:43:49 +0300

    Update `User` model username field
    - Add uniqueness validation for `User`
    - Add uniqueness constraint on `users` table

--- db/migrate/20220218071724_create_users.rb
+++ db/migrate/20220218071724_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :username, null: false
+      t.string :username, null: false, unique: true
 
       t.timestamps
     end
