5e6cd9f6126f87185e909226654092aa8c71cee2
brianorora <brianorora@gmail.com>
2022-02-24 14:42:46 +0300

    Add routes for user login and sign-up

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,12 +1,26 @@
 module Api
   class UsersController < ApplicationController
     def index
-      render json: { users: User.all }
+      render json: { users: User.all }, except: %i[created_at updated_at]
     end
 
     def show
       @user = User.find(params[:id])
-      render json: { user: @user }, except: %i[created_at updated_at]
+      render json: { user: @user }, except: %i[created_at updated_at], status: :ok
+    end
+
+    def sign_up
+      @user = User.create!(username: params[:username])
+      render json: { user: @user }, status: :ok
+    end
+
+    def login
+      @user = User.find_by(username: params[:username])
+      if @user
+        render json: { user: @user }, status: :ok
+      else
+        render json: { errors: [{ message: 'user doesn\'t exist' }] }, status: unprocessable_entity
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :horses
   namespace :api, defaults: {format: :json} do
     resources :users do
       resources :reservations 
@@ -8,5 +9,8 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resources :books 
   end
+
+  post '/login', to: 'api/users#login'
+  post '/sign_up', to: 'api/users#sign_up'
 
 end
