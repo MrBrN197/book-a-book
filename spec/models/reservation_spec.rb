a7ea12fa0cf728d5388730cbf04e689cdd65afec
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-03-02 15:35:02 +0100

    Add attributes to book created for the reservations

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -6,7 +6,12 @@ RSpec.describe Reservation, type: :model do
     book = Book.new(
       title: 'Mind Prey',
       author: 'John Sandford',
-      image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435027890l/37298.jpg'
+      image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435027890l/37298.jpg',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Pellentesque blandit imperdiet quam, nec mattis purus semper eget. Aliquam vel varius ipsum.',
+      price: 20.5,
+      rating: 3.5,
+      genre: 'Action'
     )
     Reservation.create!(
       city: 'Jos',
